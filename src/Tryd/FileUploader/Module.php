52fc68ac69fe151bd5b8217402fc154d6ac3133c
Yassel Avila <yassel.avila@gmail.com>
2013-10-31 16:34:30 -0400

    Update license information

--- src/Tryd/FileUploader/Module.php
+++ src/Tryd/FileUploader/Module.php
@@ -3,7 +3,7 @@
  * TrydFileUploader
  *
  * @copyright Copyright (c) 2013, Yassel Avila Gil (http://yasselavila.com/)
- * @license   http://www.opensource.org/licenses/bsd-license.php
+ * @license   http://tryd.net/license/new-bsd New BSD License
  * @link      http://tryd.net/projects/trydfileuploader
  */
 
