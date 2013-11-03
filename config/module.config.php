6a9a3a432fd422707475a5d167e1cc812cdc7c40
Yassel Avila <yassel.avila@gmail.com>
2013-11-03 15:49:59 -0500

    Add initial views

--- config/module.config.php
+++ config/module.config.php
@@ -9,4 +9,14 @@ return array(
             'Tryd\FileUploader\Form\Upload' => false,
         ),
     ),
+
+    'view_manager' => array(
+        'template_map' => array(
+            /* Views */
+            'tryd-fileuploader/upload/upload' => __DIR__ . '/../view/tryd-fileuploader/upload/upload.phtml',
+        ),
+        'template_path_stack' => array(
+            'Tryd\FileUploader' => __DIR__ . '/../view',
+        ),
+    ),
 );
