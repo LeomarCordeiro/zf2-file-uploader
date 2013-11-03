6f6e77893d33a71ba78e9a86cef2638547787867
Yassel Avila <yassel.avila@gmail.com>
2013-11-02 23:22:16 -0400

    Add custom RenameUpload filter

---
+++ src/Tryd/FileUploader/Filter/File/RenameUpload.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * TrydFileUploader
+ *
+ * @copyright Copyright (c) 2013, Yassel Avila Gil (http://yasselavila.com/)
+ * @license   http://tryd.net/license/new-bsd New BSD License
+ * @link      http://tryd.net/projects/trydfileuploader
+ */
+
+namespace Tryd\FileUploader\Filter\File;
+
+use Zend\Filter\File\RenameUpload as BaseRenameUpload;
+
+class RenameUpload extends BaseRenameUpload
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function applyRandomToFilename($source, $filename)
+    {
+        $filenameInfo = pathinfo($filename);
+        $sourceInfo = pathinfo($source);
+
+        $filename = $filenameInfo['filename'];
+        $filename = empty($filename) ? uniqid() : ($filename . uniqid('-'));
+
+        $extension = '';
+
+        if ($this->getUseUploadExtension() && isset($sourceInfo['extension'])) {
+            $extension = '.' . strtolower($sourceInfo['extension']);
+        } elseif (isset($filenameInfo['extension'])) {
+            $extension = '.' . strtolower($filenameInfo['extension']);
+        }
+
+        $ret = $filename . $extension;
+
+        return $ret;
+    }
+}
