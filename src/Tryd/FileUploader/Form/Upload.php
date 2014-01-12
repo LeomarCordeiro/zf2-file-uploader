190b9e86d28746a904e9ca6e0c8e5aeb08989a12
Yassel Avila <yassel.avila@gmail.com>
2014-01-12 18:33:00 -0500

    Update copyright notice

--- src/Tryd/FileUploader/Form/Upload.php
+++ src/Tryd/FileUploader/Form/Upload.php
@@ -2,7 +2,7 @@
 /**
  * TrydFileUploader
  *
- * @copyright Copyright (c) 2013, Yassel Avila Gil (http://yasselavila.com/)
+ * @copyright Copyright (c) 2013 - 2014, Yassel Avila (http://yasselavila.com/)
  * @license   http://tryd.net/license/new-bsd New BSD License
  * @link      http://tryd.net/projects/trydfileuploader
  */
