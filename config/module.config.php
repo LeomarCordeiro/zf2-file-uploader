66eaaf9759795908774dd39e9ca08a17426a16be
Yassel Avila <yassel.avila@gmail.com>
2014-01-12 18:31:59 -0500

    Update array notation

--- config/module.config.php
+++ config/module.config.php
@@ -1,79 +1,79 @@
 <?php
-return array(
-    'service_manager' => array(
-        'factories' => array(
+return [
+    'service_manager' => [
+        'factories' => [
             /* Forms */
             'Tryd\FileUploader\Form\Upload' => 'Tryd\FileUploader\Form\Service\UploadFormFactory',
-        ),
-        'shared' => array(
+        ],
+        'shared' => [
             'Tryd\FileUploader\Form\Upload' => false,
-        ),
-    ),
+        ],
+    ],
 
-    'controllers' => array(
-        'invokables' => array(
+    'controllers' => [
+        'invokables' => [
             'Tryd\FileUploader\Controller\Upload' => 'Tryd\FileUploader\Controller\UploadController',
-        ),
-    ),
+        ],
+    ],
 
-    'controller_plugins' => array(
-        'invokables' => array(
+    'controller_plugins' => [
+        'invokables' => [
             'trydFileUploader' => 'Tryd\FileUploader\Controller\Plugin\PluginProxy',
-        ),
-    ),
+        ],
+    ],
 
-    'view_helpers' => array(
-        'invokables' => array(
+    'view_helpers' => [
+        'invokables' => [
             'trydFileUploader' => 'Tryd\FileUploader\View\Helper\HelperProxy',
-        ),
-    ),
+        ],
+    ],
 
-    'view_manager' => array(
-        'template_map' => array(
+    'view_manager' => [
+        'template_map' => [
             /* Views */
             'tryd-fileuploader/upload/upload' => __DIR__ . '/../view/tryd-fileuploader/upload/upload.phtml',
             /* Views for Widgets */
             'tryd-fileuploader/widget/dnd' => __DIR__ . '/../view/tryd-fileuploader/widget/dnd.phtml',
-        ),
-        'template_path_stack' => array(
+        ],
+        'template_path_stack' => [
             'Tryd\FileUploader' => __DIR__ . '/../view',
-        ),
-    ),
+        ],
+    ],
 
-    'tryd_i18n' => array(
-        'translator' => array(
-            'translation_file_patterns' => array(
-                array(
+    'tryd_i18n' => [
+        'translator' => [
+            'translation_file_patterns' => [
+                [
                     'type'     => 'gettext',
                     'base_dir' => __DIR__ . '/../language',
                     'pattern'  => '%s.mo',
                     'text_domain' => 'Tryd\FileUploader',
-                ),
-            ),
-        ),
-    ),
+                ],
+            ],
+        ],
+    ],
 
-    'asset_manager' => array(
-        'resolver_configs' => array(
-            'paths' => array(
+    'asset_manager' => [
+        'resolver_configs' => [
+            'paths' => [
                 'Tryd\FileUploader' => __DIR__ . '/../public',
-            ),
-            'collections' => array(
-                'asset/file-uploader/css/all.css' => array(
+            ],
+            'collections' => [
+                'asset/file-uploader/css/all.css' => [
                     'asset/file-uploader/css/file-uploader.css',
-                ),
-                'asset/file-uploader/js/all.js' => array(
+                ],
+                'asset/file-uploader/js/all.js' => [
                     'asset/file-uploader/js/file-uploader.js',
-                ),
-            ),
-            'map' => array(
+                ],
+            ],
+            'map' => [
                 /* Styles */
                 'asset/file-uploader/css/file-uploader.css' => __DIR__ . '/../public/css/file-uploader.css',
                 /* Scripts */
                 'asset/file-uploader/js/file-uploader.js' => __DIR__ . '/../public/js/file-uploader.js',
                 /* Images */
                 'asset/file-uploader/img/file-add.png' => __DIR__ . '/../public/img/file-add.png',
-            ),
-        ),
-    ),
-);
+            ],
+        ],
+    ],
+];
