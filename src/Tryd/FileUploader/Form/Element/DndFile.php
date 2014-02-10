190a83e226c26e3552c6bf57ee4e79af0a897d27
Yassel Avila <yassel.avila@gmail.com>
2014-02-10 14:51:09 -0500

    Add the DndFile form element

---
+++ src/Tryd/FileUploader/Form/Element/DndFile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * TrydFileUploader
+ *
+ * @copyright Copyright (c) 2013 - 2014, Yassel Avila (http://yasselavila.com/)
+ * @license   http://tryd.net/license/new-bsd New BSD License
+ * @link      http://tryd.net/project/trydfileuploader
+ */
+
+namespace Tryd\FileUploader\Form\Element;
+
+use Zend\Form\Element\Hidden;
+
+class DndFile extends Hidden
+{
+}
